f13e2ba5025f6ca8cea8c66ae5521348ba79f6b1
Andrea <ggandream24@gmail.com>
2026-06-30 14:29:38 -0600

    fix: refactor challenge 1

--- app.js
+++ app.js
@@ -1,2 +1,4 @@
 import reto1 from "./reto1.js"; //Solo se puede un default
 import { reto2 as reto_2 } from "./reto2.js";
+
+reto1();
--- reto1.js
+++ reto1.js
@@ -1,25 +1,24 @@
 import { BASE_URL } from "./config.js";
 
-export default function ejemplo() {}
+export default function reto1() {
+  const $tabsContainer = document.querySelector(".tabs");
 
-const $tabsContainer = document.querySelector(".tabs");
+  // FETCH al enpoint
+  fetch(BASE_URL + "/challenges/reto1", { credentials: "include" })
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error("No connection, something went wrong");
+      }
+      return response.json();
+    })
+    .then((data) => {
+      markupC1(data);
+    })
+    .catch((error) => console.error("Error", error));
 
-// FETCH al enpoint
-fetch(BASE_URL + "/challenges/reto1", { credentials: "include" })
-  .then((response) => {
-    if (!response.ok) {
-      throw new Error("No connection, something went wrong");
-    }
-    return response.json();
-  })
-  .then((data) => {
-    markupC1(data);
-  })
-  .catch((error) => console.error("Error", error));
-
-function markupC1(data) {
-  const tabsHTML = data.tabs.map((tab) => {
-    return `<button
+  function markupC1(data) {
+    const tabsHTML = data.tabs.map((tab) => {
+      return `<button
             class="tab"
             role="tab"
             aria-selected="false"
@@ -30,10 +29,10 @@ function markupC1(data) {
           >
             ${tab.label}
           </button>`;
-  });
+    });
 
-  const tabpanelsHTML = data.tabs.map((tabpanel) => {
-    return `
+    const tabpanelsHTML = data.tabs.map((tabpanel) => {
+      return `
           <div
             role="tabpanel"
             class="tabpanel"
@@ -47,11 +46,11 @@ function markupC1(data) {
               <p>${tabpanel.content}</p>
             </section>
           </div>`;
-  });
+    });
 
-  $tabsContainer.insertAdjacentHTML(
-    "beforeend",
-    `<div class="tablist" role="tablist">
+    $tabsContainer.insertAdjacentHTML(
+      "beforeend",
+      `<div class="tablist" role="tablist">
     ${tabsHTML.join(" ")}
     <div class="tab__indicator"></div>
     </div>
@@ -59,77 +58,78 @@ function markupC1(data) {
     ${tabpanelsHTML.join(" ")}
     </div>  
     `,
-  );
+    );
 
-  const $tabs = $tabsContainer.querySelectorAll(".tab");
-  const $tabpanels = $tabsContainer.querySelectorAll(".tabpanel");
+    const $tabs = $tabsContainer.querySelectorAll(".tab");
+    const $tabpanels = $tabsContainer.querySelectorAll(".tabpanel");
 
-  showSelected(0, false);
+    showSelected(0, false);
 
-  $tabsContainer.addEventListener("click", (e) => {
-    if (e.target.matches(".tab")) {
+    $tabsContainer.addEventListener("click", (e) => {
+      if (e.target.matches(".tab")) {
+        removeSelected();
+
+        const indexOfSelected = Array.from($tabpanels).findIndex(
+          (tabpanel) =>
+            tabpanel.getAttribute("id") ===
+            e.target.getAttribute("aria-controls"),
+        );
+
+        showSelected(indexOfSelected, false);
+      }
+    });
+
+    $tabsContainer.addEventListener("keydown", (e) => {
+      const currentTab = e.target;
+      const index = Array.from($tabs).indexOf(currentTab);
+
+      if (index === -1) return;
+
+      let newIndex = 0;
+      switch (e.key) {
+        case "ArrowLeft":
+          newIndex = (index - 1 + $tabs.length) % $tabs.length;
+          break;
+        case "ArrowRight":
+          newIndex = (index + 1) % $tabs.length;
+          break;
+        case "Home":
+          newIndex = 0;
+          break;
+        case "End":
+          newIndex = $tabs.length - 1;
+          break;
+        default:
+          return;
+      }
+
       removeSelected();
 
-      const indexOfSelected = Array.from($tabpanels).findIndex(
-        (tabpanel) =>
-          tabpanel.getAttribute("id") ===
-          e.target.getAttribute("aria-controls"),
-      );
+      e.preventDefault(); // Cancela la acción nativa de la tecla
+      e.stopPropagation(); //Evita que el evento suba a elementos padre
 
-      showSelected(indexOfSelected, false);
-    }
-  });
-
-  $tabsContainer.addEventListener("keydown", (e) => {
-    const currentTab = e.target;
-    const index = Array.from($tabs).indexOf(currentTab);
-
-    if (index === -1) return;
-
-    let newIndex = 0;
-    switch (e.key) {
-      case "ArrowLeft":
-        newIndex = (index - 1 + $tabs.length) % $tabs.length;
-        break;
-      case "ArrowRight":
-        newIndex = (index + 1) % $tabs.length;
-        break;
-      case "Home":
-        newIndex = 0;
-        break;
-      case "End":
-        newIndex = $tabs.length - 1;
-        break;
-      default:
-        return;
-    }
-
-    removeSelected();
-
-    e.preventDefault(); // Cancela la acción nativa de la tecla
-    e.stopPropagation(); //Evita que el evento suba a elementos padre
-
-    showSelected(newIndex, true);
-  });
-
-  function removeSelected() {
-    $tabpanels.forEach((tabpanel) => {
-      tabpanel.setAttribute("hidden", "true");
+      showSelected(newIndex, true);
     });
 
-    $tabs.forEach((tab) => {
-      tab.setAttribute("tabindex", "-1");
-      tab.setAttribute("aria-selected", "false");
-    });
-  }
+    function removeSelected() {
+      $tabpanels.forEach((tabpanel) => {
+        tabpanel.setAttribute("hidden", "true");
+      });
 
-  function showSelected(i, isFocus) {
-    $tabpanels[i].removeAttribute("hidden");
-    $tabs[i].setAttribute("tabindex", "0");
-    $tabs[i].setAttribute("aria-selected", "true");
+      $tabs.forEach((tab) => {
+        tab.setAttribute("tabindex", "-1");
+        tab.setAttribute("aria-selected", "false");
+      });
+    }
 
-    if (isFocus) {
-      $tabs[i].focus();
+    function showSelected(i, isFocus) {
+      $tabpanels[i].removeAttribute("hidden");
+      $tabs[i].setAttribute("tabindex", "0");
+      $tabs[i].setAttribute("aria-selected", "true");
+
+      if (isFocus) {
+        $tabs[i].focus();
+      }
     }
   }
 }
